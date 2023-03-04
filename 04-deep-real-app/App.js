b5b8d68cc6693dbc02c6980ce25ec3893abf045f
JiangShuuu <globelex65@gmail.com>
2023-03-04 15:49:43 +0800

    upd

--- 04-deep-real-app/App.js
+++ 04-deep-real-app/App.js
@@ -1,19 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient'
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <LinearGradient colors={['#ddb52f', '#4e0329']} style={styles.rootScreen}>
+      <ImageBackground source={require('./assets/images/background.png')} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  backgroundImage: {
+    opacity: 0.15
+  }
 });
