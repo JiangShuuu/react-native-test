79b4017c12102890c6a589ba793ed85bc8aa9857
JiangShuuu <globelex65@gmail.com>
2023-03-04 15:38:08 +0800

    upd

--- 04-deep-real-app/screens/StartGameScreen.js
+++ 04-deep-real-app/screens/StartGameScreen.js
@@ -5,14 +5,22 @@ export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
       <TextInput style={styles.numberInput} maxLength={2} autoCapitalize="none" autoCorrect={false} keyboardType="number-pad" />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
@@ -34,5 +42,11 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  buttonsContainer: {
+    flexDirection: 'row'
+  },
+  buttonContainer: {
+    flex: 1
   }
 })
