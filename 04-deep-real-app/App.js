9824fa561fc7356ba47908ef752be6165789f603
JiangShuuu <globelex65@gmail.com>
2023-03-18 20:32:59 +0800

    upd

--- 04-deep-real-app/App.js
+++ 04-deep-real-app/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { useState } from 'react';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import Colors from './constants/colors';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState()
@@ -18,7 +19,7 @@ export default function App() {
   }
 
   return (
-    <LinearGradient colors={['#ddb52f', '#4e0329']} style={styles.rootScreen}>
+    <LinearGradient colors={[Colors.accent500, Colors.primary700]} style={styles.rootScreen}>
       <ImageBackground source={require('./assets/images/background.png')} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
         <SafeAreaView style={styles.rootScreen}>
           {screen}
