fcc2562450036ef6436eab1b8e6e922d52899150
JiangShuuu <globelex65@gmail.com>
2023-03-24 22:22:03 +0800

    upd

--- 05-meals-app/screens/MealsOverviewScreen.js
+++ 05-meals-app/screens/MealsOverviewScreen.js
@@ -1,10 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import { MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
+// import { useRoute } from '@react-navigation/native'
+export default function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId
 
-export default function MealsOverviewScreen() {
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0
+  })
+
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Mel</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   )
 }
