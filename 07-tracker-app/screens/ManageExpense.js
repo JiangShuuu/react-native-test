f5f481d0ff622d685830fb6f0bb35e9b05e4d518
JiangShuuu <globelex65@gmail.com>
2023-04-09 15:36:33 +0800

    upd

--- 07-tracker-app/screens/ManageExpense.js
+++ 07-tracker-app/screens/ManageExpense.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { GlobalStyles } from '../constants/styles'
+import IconButton from '../components/UI/IconButton'
+import Button from '../components/UI/Button'
 
 export default function ManageExpense({ navigation, route }) {
   const editedExpenseId = route.params?.expenseId
@@ -12,5 +15,55 @@ export default function ManageExpense({ navigation, route }) {
     })
   }, [navigation, isEditing])
 
-  return <Text>ManageExpense</Text>
+  const deleteExpenseHandler = () => {}
+
+  const cancelHandler = () => {}
+  const confirmHandler = () => {}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'update' : 'Add'}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+})
